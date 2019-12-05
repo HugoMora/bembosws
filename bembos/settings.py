7cb9709c115dc15f593b7485129f72db1b03ef0e
HugoMora <37921016+HugoMora@users.noreply.github.com>
2019-12-05 08:58:07 -0500

    12

--- bembos/settings.py
+++ bembos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8p)#k!y26hdppz%v-=4mpa(5q(en9ae86ny%jb@&g-pefbxsod'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
